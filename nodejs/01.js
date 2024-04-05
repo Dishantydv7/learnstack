46a19a2dc07fde4d5f9d563808ff807fc7257422
Dishant Yadav <dishantrao11@gmail.com>
2024-04-05 18:30:19 +0530

    first professional commit

---
+++ nodejs/01.js
@@ -0,0 +1,18 @@
+console.log("hello their");
+
+
+// 5000 is in millisec
+// setInterval(() => {
+//     console.log("hello tier ");
+// }, 5000)
+
+
+
+//Modules variable
+
+// const sayhi = (name) => {
+//     console.log(`Hello , ${name}`);
+// }
+
+// sayhi("dihant")
+
